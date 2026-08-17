42aa107e3c43e73212f0b096e61b446979f072e2
suneel <suneelchalla444@gmail.com>
2026-08-17 12:14:56 +0530

    message

--- 01.variables.sh
+++ 01.variables.sh
@@ -2,7 +2,7 @@
 
 PERSON1=suneel
 PERSON2=harika
-    echo "suneel : hi harika how are you"
-    echo "harika : hello suneel i am fine and how are you"
-    echo "suneel : i am fine what are you doing now"
-    echo "harika : i got married i have a baby boy his name is suneel"
+    echo "$PERSON1 : hi $PERSON2 how are you"
+    echo "$PERSON2 : hello $PERSON1 i am fine and how are you"
+    echo "$PERSON1 : i am fine what are you doing now"
+    echo "$PERSON2 : i got married i have a baby boy his name is $PERSON1"
