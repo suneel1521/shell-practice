c7cd34a7e485f65001f1f3d216b718f92d09d861
suneel <suneelchalla444@gmail.com>
2026-08-17 12:01:36 +0530

    added conversition

--- 01.variables.sh
+++ 01.variables.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ]
-then
-    echo "Error :: Please run the script with sudo access"
-    exit 1
-else
-    echo "You are running with root access"
-fi
-
-dnf list installed mysql
-
-if [ $? -ne 0 ]
-then
-    echo "MySQL is not installed.....going to install"
-
-    dnf install mysql -y
-
-    if [ $? -eq 0 ]
-    then
-        echo "Installing MySQL.....success"
-    else
-        echo "MySQL installation.....failure"
-        exit 1
-    fi
-else
-    echo "MySQL is already installed, nothing to do"
-fi
+PERSON1=suneel
+PERSON2=harika
+    echo "suneel : hi harika how are you"
+    echo "harika : hello suneel i am fine and how are you"
+    echo "suneel : i am fine what are you doing now"
+    echo "harika : i got married i have a baby boy his name is suneel"
