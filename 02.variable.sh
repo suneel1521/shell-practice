0600137600ec5d40083f806f6dd9aeebaa966dc2
suneel <suneelchalla444@gmail.com>
2026-08-17 12:17:59 +0530

    message

---
+++ 02.variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+    echo "$PERSON1 : hi $PERSON2 how are you"
+    echo "$PERSON2 : hello $PERSON1 i am fine and how are you"
+    echo "$PERSON1 : i am fine what are you doing now"
+    echo "$PERSON2 : i got married i have a baby boy his name is $PERSON1"
