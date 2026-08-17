a7cda310e118e2bc4c183e3a75a0a320120634ee
suneel <suneelchalla444@gmail.com>
2026-08-17 17:51:16 +0530

    installing mysql packages

---
+++ 05.functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ 
+USERID=&(id -u)
+if [ $USERID -ne 0 ]
+    echo "ERROR : this user has enough permissions enable root access to run script"
+    exit 1
+else
+    echo "you are running with root access"
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "mysql is not installed.....now getting installing"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+        echo "mysql installed.....successfully"
+    else
+        echo "mysql installed.....failure"
+    fi
+else
+    echo "mysql is already installed.....no need to change it"
+fi
