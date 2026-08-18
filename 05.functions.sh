57c1029599f65ef312956ff8097aa4558d32e58c
suneel <suneelchalla444@gmail.com>
2026-08-18 10:50:57 +0530

    changing the code

--- 05.functions.sh
+++ 05.functions.sh
@@ -25,3 +25,23 @@ then
 else
     echo "mysql is already installed.....no need be change"
 fi
+
+dnf list installed nginx
+if [ $? -eq 0 ]
+then
+    echo "nginx is not installed.....going to be installed"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "nginx is already installed.....no need to be change"
+fi
+
+dnf list installed mongodb.org
+if [ $? -eq 0 ]
+then
+    echo "nginx is not installed.....going to be installed"
+    dnf install mongobd.org -y
+    VALIDATE $? "mongodb"
+else
+    echo "mongodb is already installed.....no need to be change"
+fi
