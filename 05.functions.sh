d4a279a9f5b97da30a0d318776b85a210771d2a7
suneel <suneelchalla444@gmail.com>
2026-08-18 10:28:26 +0530

    changing the code

--- 05.functions.sh
+++ 05.functions.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
- 
+
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR : this user has enough permissions enable root access to run script"
+    echo "ERROR:you cannot proceed please login root access"
     exit 1
 else
-    echo "you are running with root access"
+    echo "you have a root access"
 fi
 
-dnf list installed mysql
+dnf list installed nginx
 if [ $? -ne 0 ]
 then
-    echo "mysql is not installed.....now getting installing"
-    dnf install mysql -y
+    echo "nginx is not instaled.....going to be installing"
+    dnf install nginx -y
     if [ $? -eq 0 ]
     then
-        echo "mysql installed.....successfully"
+        echo "nginx installed.....successuful"
     else
-        echo "mysql installed.....failure"
+        echo "nginx installing.....failure"
     fi
 else
-    echo "mysql is already installed.....no need to change it"
+    echo "nginx is already installed.....no need be change"
 fi
